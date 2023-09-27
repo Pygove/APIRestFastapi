8818adc5365743adf5f292219477ba482be9b276
Gonzalo-Veron-24 <gonzalo.veron.unam@gmail.com>
2023-09-27 20:12:00 -0300

    "CRUD PRODUCTOS ADD"

--- routers/users_db.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, HTTPException, status
-from models.user import User
-from schemas.user import user_schema, users_schema
-from bson import ObjectId
-
-router = APIRouter()
